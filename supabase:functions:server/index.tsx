c0d565c33cd88d2e690e660e14f493344c9a2470
pavanaval-commits <pavan.aval@gmail.com>
2025-09-23 16:10:11 -0400

    Delete supabase:functions:server directory

--- supabase:functions:server/index.tsx
+++
@@ -1,6 +0,0 @@
-// STUB FILE - Vercel should ignore this due to .vercelignore
-// Using mock data instead of backend
-
-export default function stub() {
-  return { message: 'Using mock data - backend not needed' };
-}
