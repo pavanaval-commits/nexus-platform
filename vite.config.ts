8b7fb1d0c455dceb1f773cc46b5746533dd13a88
pavanaval-commits <pavan.aval@gmail.com>
2025-09-23 15:22:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    port: 3000,
-    host: true
-  },
-  preview: {
-    port: 3000,
-    host: true
-  },
+  plugins: [react()],
+  resolve: { alias: { '@': '/src' } },
+  server: { port: 3000, host: true },
+  preview: { port: 3000, host: true },
   build: {
     outDir: 'dist',
     sourcemap: false,
