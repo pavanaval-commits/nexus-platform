5a6379de48b5865c27ee44068048a916fcbc991a
pavanaval-commits <pavan.aval@gmail.com>
2025-09-23 10:37:04 -0400

    Create client.ts

---
+++ utils/supabase/client.ts
@@ -0,0 +1,6 @@
+// Static Supabase configuration - no environment variables
+export const supabaseConfig = {
+  url: "https://mkknfgjcrvzklfqfuvjb.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ra25mZ2pjcnZ6a2xmcWZ1dmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1ODc3OTEsImV4cCI6MjA3NDE2Mzc5MX0.lJ3Sz0OL3XJi6iC7q1-yLKMADqVGTJ0TJirZP60PpuI",
+  projectId: "mkknfgjcrvzklfqfuvjb"
+};
