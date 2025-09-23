0b95d60f5346cf9105f3d99874b26ce70302e8f7
pavanaval-commits <pavan.aval@gmail.com>
2025-09-23 10:37:28 -0400

    Update client.ts

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -4,3 +4,6 @@ export const supabaseConfig = {
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ra25mZ2pjcnZ6a2xmcWZ1dmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1ODc3OTEsImV4cCI6MjA3NDE2Mzc5MX0.lJ3Sz0OL3XJi6iC7q1-yLKMADqVGTJ0TJirZP60PpuI",
   projectId: "mkknfgjcrvzklfqfuvjb"
 };
+
+export const projectId = supabaseConfig.projectId;
+export const publicAnonKey = supabaseConfig.anonKey;
