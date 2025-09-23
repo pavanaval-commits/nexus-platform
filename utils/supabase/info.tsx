ffb84bc2010e648a17cebe809f7ee72227925509
pavanaval-commits <pavan.aval@gmail.com>
2025-09-23 10:06:52 -0400

    Update info.tsx

--- utils/supabase/info.tsx
+++ utils/supabase/info.tsx
@@ -1,4 +1,3 @@
-tsx
 // Environment variables for deployment
 export const projectId = "mkknfgjcrvzklfqfuvjb"
 export const publicAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ra25mZ2pjcnZ6a2xmcWZ1dmpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1ODc3OTEsImV4cCI6MjA3NDE2Mzc5MX0.lJ3Sz0OL3XJi6iC7q1-yLKMADqVGTJ0TJirZP60PpuI"
