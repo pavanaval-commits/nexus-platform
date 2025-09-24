8e512ceb29ef9a02e571cd5bf530b2a8a10c0100
pavanaval-commits <pavan.aval@gmail.com>
2025-09-24 16:26:11 -0400

    chore: eslint+prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,1 @@
+module.exports={parser:'@typescript-eslint/parser',plugins:['@typescript-eslint','react','react-hooks'],extends:['plugin:react/recommended','plugin:react-hooks/recommended','plugin:@typescript-eslint/recommended','prettier'],settings:{react:{version:'detect'}},env:{browser:true,es2021:true},ignorePatterns:['dist','node_modules']}
